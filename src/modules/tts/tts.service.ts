5d9e2655a9e9a8b94b6351b950d194d1d65d3af7
Zeus-s <kristabdixx@gmail.com>
2023-12-14 20:42:51 +0700

    fix: path

--- src/modules/tts/tts.service.ts
+++ src/modules/tts/tts.service.ts
@@ -9,7 +9,7 @@ export class TtsService {
     private storage: Storage;
     private bucketName = process.env.BUCKET_NAME;
     private audioFolder = 'audio';
-    private localPath = `/usr/src/app/assets/${this.audioFolder}`;
+    private localPath = `usr/src/app/assets/${this.audioFolder}`;
 
     constructor() { 
         this.storage = new Storage();
@@ -19,7 +19,7 @@ export class TtsService {
         try {
             const baseName = tts.text.toLowerCase();
             const fileName = `${baseName}.mp3`;
-            const localFile = `${this.localPath}/${baseName}`
+            const localFile = `${this.localPath}/${fileName}`
             const fileExists = await this.checkFileExists(fileName);
 
             if (fileExists) {
